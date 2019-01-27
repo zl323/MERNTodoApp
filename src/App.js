8d64cededb6ff77880ea3bb44b9ecf442bfacac8
Zhiyuan Li <zhiyuanli@Zhiyuans-MacBook-Pro.local>
2019-01-26 23:34:35 -0500

    todo, edit and create list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
 class App extends Component {
   render() {
     return (
-     <div className="container">
-      <h2>ToDoApp</h2>
-     </div>
+      <Router>
+        <div className="container">
+          <h2>ToDoApp</h2>
+          <Route path = "/" exact component={TodosList} />
+          <Route path = "/edit/:id" component={EditTodo} />
+          <Route path = "/create" component={CreateTodo} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/todos-list.component.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+export default class TodosList extends Component {
+    render(){
+        return(
+            <div>
+                <p>Welcome to Todos List Component!</p>
+            </div>
+        )
+    }
+}
